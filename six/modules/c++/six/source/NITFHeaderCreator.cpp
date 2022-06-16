3c1f951489ac59decaaad66fd88e4507ff89bcd0
J. Daniel Smith <dan@jdanielsmith.me>
2022-06-16 18:18:25 -0400

    remove SICD 1.3 changes from this branch

--- six/modules/c++/six/source/NITFHeaderCreator.cpp
+++ six/modules/c++/six/source/NITFHeaderCreator.cpp
@@ -555,11 +555,6 @@ void NITFHeaderCreator::addUserDefinedSubheader(
             specVers = "2.0";
             specDT = "2019-05-31T00:00:00Z";
         }
-        else if (strVersion == "3.0.0")
-        {
-            specVers = "3.0";
-            specDT = "2021-11-30T00:00:00Z";
-        }
     }
 
     // spec version
